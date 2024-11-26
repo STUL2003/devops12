bcec3c45840d01f32906c715227618007942dd77
dima <dimasurkov2003@gmail.com>
2024-11-26 20:55:24 +0500

    workflows9

--- server/application.py
+++ server/application.py
@@ -1,20 +1,22 @@
 """import"""
 import http.server
 import socketserver
+
 PORT = 8000
+
 """class """
-class TestMe():
-"""возвращает  4"""
+class TestMe:
+    """возвращает  4"""
     def take_five(self):
         return 4
-"""возвращает порт"""
+    
+    """возвращает порт"""
     def port(self):
         return PORT
+
 """запуск сервера"""
-if __name__ == '__main__':
+if name == 'main':
     Handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", PORT), Handler) as http:
         print("serving at port", PORT)
         http.serve_forever()
-
-
